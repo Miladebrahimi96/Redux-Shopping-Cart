4b5dfadbbebd4328c9da3e867f172b693a3feb70
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-13 23:06:12 +0430

    ShopCart initialized and imported in App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Routes , Route, Navigate} from "react-router-dom";
 //components
 import Store from './components/Store';
 import ProductDetails from './components/shared/ProductDetails';
+import Navbar from './components/shared/Navbar';
+import ShopCart from './components/ShopCart';
 
 //contexts
 import ProductContextProvider from './contexts/ProductContextProvider';
 import CartContextProvider from './contexts/CartContextProvider';
-import Navbar from './components/shared/Navbar';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Routes>
           <Route path='/products' element={<Store/>} />
           <Route path='/products/:id' element={<ProductDetails/>} />
+          <Route path='/cart' element={<ShopCart />} />
           <Route path='/*' element={<Navigate to="/products" />} />
         </Routes>
       </CartContextProvider>
