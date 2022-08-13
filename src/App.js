0a542d45580ee90d62b1cb6f0a92e40305e27d79
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-13 20:56:14 +0430

    ProductDetails created and imported in app with Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes , Route, Navigate} from "react-router-dom";
 
 //components
 import Store from './components/Store';
+import ProductDetails from './components/shared/ProductDetails';
 
 //contexts
 import ProductContextProvider from './contexts/ProductContextProvider';
@@ -12,6 +13,7 @@ function App() {
     <ProductContextProvider>
       <Routes>
         <Route path='/products' element={<Store/>} />
+        <Route path='/products/:id' element={<ProductDetails/>} />
         <Route path='/*' element={<Navigate to="/products" />} />
       </Routes>
     </ProductContextProvider>
