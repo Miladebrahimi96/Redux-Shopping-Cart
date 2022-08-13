b3ed2fdc2dffa5b01acd5d42ba8400c5d76bae30
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-13 20:32:40 +0430

    A fucntion created to show only first two words of product title and used in Product

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+//Functions
+import { shorten } from '../helper/functions';
+
 const Product = ({productData}) => {
     return (
         <div>
             <img src={productData.image} alt={productData.title} style={{width:"200px"}}/>
-            <h3>{productData.title}</h3>
+            <h3>{shorten(productData.title)}</h3>
             <p>{productData.price}</p>
             <div>
                 <a href='#'>Details</a>
