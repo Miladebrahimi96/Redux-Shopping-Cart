4f397a29c962bbec3044a20cf107ca9897aeaefb
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-13 20:53:19 +0430

    defined Routes for Store

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes , Route, Navigate} from "react-router-dom";
 
 //components
 import Store from './components/Store';
@@ -9,7 +10,10 @@ import ProductContextProvider from './contexts/ProductContextProvider';
 function App() {
   return (
     <ProductContextProvider>
-      <Store />
+      <Routes>
+        <Route path='/products' element={<Store/>} />
+        <Route path='/*' element={<Navigate to="/products" />} />
+      </Routes>
     </ProductContextProvider>
   );
 }
