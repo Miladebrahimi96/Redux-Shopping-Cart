0d6c2e445dc00bf3b843e2502bd43194f870a013
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-13 21:22:48 +0430

    ProductDetails finished

--- src/components/shared/ProductDetails.js
+++ src/components/shared/ProductDetails.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { useParams } from "react-router-dom";
+
+//Contexts
+import { ProductContext } from '../../contexts/ProductContextProvider';
+import { Link } from "react-router-dom";
 
 const ProductDetails = () => {
+    
+    const params = useParams();
+    const id = params.id;
+
+    const data = useContext(ProductContext);
+    const product = data[id -1];
+    const {image, title, description, price, category} = product;
+
     return (
         <div>
-            details
+            <img src={image} alt="prdoduct" />
+            <div>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <p><span>Category:</span> {category}</p>
+                <div>
+                    <span>{price} $</span>
+                    <Link to="/products">Back to Shop</Link>
+                </div>
+            </div>
         </div>
     );
 };
